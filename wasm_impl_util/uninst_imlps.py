fb4b45034286df4ab3b83b31ca6eb8f4d597a8f7
erxiaozhou <651327272@qq.com>
2023-04-02 23:09:07 +0800

    update code of uninst_runtime

--- wasm_impl_util/uninst_imlps.py
+++ wasm_impl_util/uninst_imlps.py
@@ -1,4 +1,5 @@
 from file_util import combine_path
+from file_util import byte2str
 import subprocess
 
 
@@ -8,32 +9,43 @@ class uninst_runtime:
         self.name = name
         self.cmd_fmt = cmd_fmt
         self.err_channel = err_channel
-    
-    @classmethod
-    def from_dict(cls, name, dict_):
-        bin_path = combine_path(dict_['standard_dir'], dict_['bin_relative_path'])
-        cmd_fmt = dict_['cmd'].format(bin_path, '{}')
-        if ' 2>' in dict_['dump_cmd']:
-            err_channel = 'stderr'
-        else:
-            assert ' >' in dict_['dump_cmd']
-            err_channel = 'stdout'
-        return cls(name, cmd_fmt, err_channel)
 
     def execute(self, wasm_path):
         cmd = self.cmd_fmt.format(wasm_path)
         p = subprocess.Popen(cmd, stderr=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
+        out_byte_content = byte2str(p.stdout.read()).strip(' \n\t')
+        err_byte_content = byte2str(p.stderr.read()).strip(' \n\t')
         if self.err_channel == 'stdout':
-            out_byte_content = p.stdout.read()
-            try: 
-                content = out_byte_content.decode()
-            except Exception:
-                content = str(out_byte_content)
+            content = out_byte_content
+            if len(err_byte_content):
+                content += '\n' + err_byte_content
         else:
-            err_byte_content = p.stderr.read()
-            assert self.err_channel == 'stderr'
-            try: 
-                content = err_byte_content.decode()
-            except Exception:
-                content = str(err_byte_content)
+            content = err_byte_content
         return content
+    
+    @classmethod
+    def from_std_dict(cls, name, dict_):
+        cmd_fmt, err_channel = _get_paras_from_dict(dict_, 'standard_dir')
+        return cls(name, cmd_fmt, err_channel)
+    
+    @classmethod
+    def from_lastest_dict(cls, name, dict_):
+        cmd_fmt, err_channel = _get_paras_from_dict(dict_, 'lastest_dir')
+        return cls(name, cmd_fmt, err_channel)
+
+
+def _get_paras_from_dict(dict_, runtime_dir_name):
+    assert runtime_dir_name in ['standard_dir', 'lastest_dir']
+    bin_path = combine_path(dict_[runtime_dir_name], dict_['bin_relative_path'])
+    cmd_fmt = dict_['cmd'].format(bin_path, '{}')
+    err_channel = detect_channel(dict_['dump_cmd'])
+    return cmd_fmt, err_channel
+
+
+def detect_channel(dump_cmd):
+    if ' 2>' in dump_cmd:
+        err_channel = 'stderr'
+    else:
+        assert ' >' in dump_cmd
+        err_channel = 'stdout'
+    return err_channel
