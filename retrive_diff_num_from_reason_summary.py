38a15d0cf899679b10ca2d7c6a0e1e9940808af1
erxiaozhou <651327272@qq.com>
2023-10-05 21:18:39 +0800

    fix rq2 files

--- retrive_diff_num_from_reason_summary.py
+++ retrive_diff_num_from_reason_summary.py
@@ -27,39 +27,26 @@ class reasonRelatedC:
         self.reason_summary = read_json(self.reson_file_path)
         self.ctgy_info = read_json('./retrive_diff_num_from_reason_summary_util/inst2ctgys.json')
 
-
-    def check_all_tc_num(self):
-        num_from_diff_dir = dir_file_num(self.diff_dir)
-        num_from_reason_file = _all_tc_num_in_reason_file(self.reson_file_path)
-        assert num_from_diff_dir == num_from_reason_file, print(num_from_diff_dir, num_from_reason_file)
-        print(num_from_reason_file)
-    
     def count_exec_state_diff(self, ctgy_name=None, get_ori_names=True):
         assert get_ori_names
         not_sure_keys, runtime_counter, to_skip_list, to_skip_num = self.only_can_exec_state_by_key(ctgy_name, get_ori_names)
         compare_based_c = self.count_by_compare_exec_state(not_sure_keys, ctgy_name, get_ori_names)
-        for k in all_runtime_names:
-            if k not in runtime_counter:
-                runtime_counter[k] = set()
-            runtime_counter[k].update(compare_based_c.get(k, set()))
-        if ctgy_name == 'Memory':
-            print(runtime_counter['wasm3_dump'])
-            # assert 0
-        #     print('==============================')
-        #     for k, v in runtime_counter.items():
-        #         print(k, v)
-        #     assert 0
+        if get_ori_names:
+
+            for k in all_runtime_names:
+                if k not in runtime_counter:
+                    runtime_counter[k] = set()
+                runtime_counter[k].update(compare_based_c.get(k, set()))
+        else:
+            runtime_counter+=compare_based_c
         return runtime_counter, to_skip_num
 
 
     def count_c_diff(self, ctgy_name=None, get_ori_names=False):
         not_sure_keys, runtime_counter, to_skip_list, to_skip_num = self.only_can_cannot_execute(ctgy_name, get_ori_names)
-        if ctgy_name == 'Numeric':
-            print('*******' *30)
-            # assert 0, print(runtime_counter['wasmer_default_dump'])
         compare_based_c = self.count_by_compare(not_sure_keys, ctgy_name, get_ori_names)
         if get_ori_names:
-            
+
             for k in all_runtime_names:
                 if k not in runtime_counter:
                     runtime_counter[k] = set()
@@ -69,7 +56,6 @@ class reasonRelatedC:
         return runtime_counter, to_skip_num
 
     def only_can_cannot_execute(self, ctgy_name=None, get_ori_names=False):
-        # not_sure_list = []
         not_sure_keys = []
         to_skip_list = []
         to_skip_num = 0
@@ -82,16 +68,10 @@ class reasonRelatedC:
             if 'CanRun_CannotDump' in k:
                 to_skip_list.append(k)
                 continue
-            
-            # elif k.count('has_timeout') != k.count("('WasmEdge_disableAOT_newer', ('has_timeout',))"):
-            #     not_sure_keys.append(k)
-            #     to_skip_num += tc_num
-            #     continue
             k_obj = key_(k)
             other_kwds = [v for v in k_obj.kwds_list if v not in execute_related_tuple]
             execute_related_keys = [v for v in k_obj.kwds_list if v in execute_related_tuple]
             assert len(other_kwds) + len(execute_related_keys) > 0
-            # if (len(other_kwds) <= 3 and len(execute_related_keys) == 0) or (len(execute_related_keys)  <=3 and len(other_kwds) == 0):
             if (len(execute_related_keys)  <=3 and len(other_kwds) == 0):
                 print(f'only_can_cannot_execute == 3 num rule: {k}')
                 for runtime_name in k_obj.runtime_names:
@@ -128,7 +108,6 @@ class reasonRelatedC:
                         else:
                             runtime_counter[runtime_name] += tc_num
                 continue
-            print('?????????????????????????????')
             not_sure_keys.append(k)
         if get_ori_names:
             return not_sure_keys, name_dict, to_skip_list, to_skip_num
@@ -185,80 +164,14 @@ class reasonRelatedC:
                         else:
                             runtime_counter[runtime_name] += tc_num
                 continue
-            print('?????????????????????????????')
             not_sure_keys.append(k)
         if get_ori_names:
             return not_sure_keys, name_dict, to_skip_list, to_skip_num
         else:
             return not_sure_keys, runtime_counter, to_skip_list, to_skip_num
-        
-    def only_can_cannot_execute_ori(self, ctgy_name=None, get_ori_names=False):
-        # not_sure_list = []
-        not_sure_keys = []
-        to_skip_list = []
-        to_skip_num = 0
-        runtime_counter = Counter()
-        name_dict = dict()
-        execute_related_tuple = set(['CanExecute', 'CannotExecute'])
-        for k, v in self.reason_summary.items():
-            tc_num = self.count_tc_num_with(v, ctgy_name)
-            tcs = self.tc_num_with(v, ctgy_name)
-            if 'CanRun_CannotDump' in k:
-                to_skip_list.append(k)
-                continue
-            
-            elif k.count('has_timeout') != k.count("('WasmEdge_disableAOT_newer', ('has_timeout',))"):
-                not_sure_keys.append(k)
-                to_skip_num += tc_num
-                continue
-            k_obj = key_(k)
-            other_kwds = [v for v in k_obj.kwds_list if v not in execute_related_tuple]
-            execute_related_keys = [v for v in k_obj.kwds_list if v in execute_related_tuple]
-            if len(other_kwds) <= 3 and len(execute_related_keys)  <=3:
-                print(f'3 num rule: {k}')
-                for runtime_name in k_obj.runtime_names:
-                    if get_ori_names:
-                        if runtime_name not in name_dict:
-                            name_dict[runtime_name] = set()
-                        name_dict[runtime_name].update(tcs)
-                    else:
-                        runtime_counter[runtime_name] += tc_num
-                continue
-            elif 'stack_bytes_process_nan' in k:
-                not_sure_keys.append(k)
-                continue
-            
-            elif k_obj.kws.issubset(execute_related_tuple):
-                assert len(k_obj.kws) == 1, print(k_obj.kws, k_obj.data)
-                if len(k_obj.kwds_list) <=3:
-                    print(f'Cannot Can rule: {k}')
-                    for runtime_name in k_obj.runtime_names:
-                        if get_ori_names:
-                            if runtime_name not in name_dict:
-                                name_dict[runtime_name] = set()
-                            name_dict[runtime_name].update(tcs)
-                        else:
-                            runtime_counter[runtime_name] += tc_num
-                else:
-                    print(f'Cannot Can revise rule: f{k}')
-                    revised_list =[v for v in all_runtime_names if v not in k_obj.runtime_names]
-                    for runtime_name in revised_list:
-                        if get_ori_names:
-                            if runtime_name not in name_dict:
-                                name_dict[runtime_name] = set()
-                            name_dict[runtime_name].update(tcs)
-                        else:
-                            runtime_counter[runtime_name] += tc_num
-                continue
-            print('?????????????????????????????')
-            not_sure_keys.append(k)
-        if get_ori_names:
-            return not_sure_keys, name_dict, to_skip_list, to_skip_num
-        else:
-            return not_sure_keys, runtime_counter, to_skip_list, to_skip_num
+       
 
     def count_na_diff(self, ctgy_name=None, get_ori_names=False):
-        # not_sure_list = []
         not_sure_keys = []
         to_skip_num = 0
         runtime_counter = Counter()
@@ -274,7 +187,6 @@ class reasonRelatedC:
             kwds = k_obj.kwds_list
             if 'stack_bytes_process_nan' not in kwds:
                 continue
-            # infer can run num
             if 'CanExecute' in kwds:
                 can_run_num = kwds.count('CanExecute')
                 can_run_runtimes = [_k for _k,_v in k_obj.data.items() if 'CanExecute' in _v]
@@ -296,7 +208,7 @@ class reasonRelatedC:
             return name_dict
         else:
             return runtime_counter
-            
+
     def count_by_compare_nan(self, key, th, ctgy=None, get_ori_names=False, can_run_runtimes=None):
         name_p = re.compile(r'^(.*)_\d+$')
         skp_list = read_json('./retrive_diff_num_from_reason_summary_util/to_skip_names.json')
@@ -343,7 +255,6 @@ class reasonRelatedC:
         else:
             return c
     def count_by_compare_exec_state(self, keys, ctgy=None, get_ori_names=False):
-        # assert 0, print(ctgy)
         name_p = re.compile(r'^(.*)_\d+$')
         c = Counter()
         name_dict = dict()
@@ -357,7 +268,6 @@ class reasonRelatedC:
         for tc in tqdm(tcs):
             dumped_dir = self.dumped_data_dir / tc
             names = analyze_a_tc_exec_state(dumped_dir)
-            # assert not len(names)
             for name in names:
                 c[name] += 1
                 if name not in name_dict:
@@ -386,7 +296,7 @@ class reasonRelatedC:
                 if self.ctgy_info[name] == ctgy:
                     inst_names.add(name)
             return inst_names
-        
+
     def count_tc_num_with(self, tc_names, ctgy):
         skp_list = read_json('./retrive_diff_num_from_reason_summary_util/to_skip_names.json')
         name_p = re.compile(r'^(.*)_\d+$')
@@ -421,7 +331,6 @@ class reasonRelatedC:
             can_execute_keys = [_k for _k, _v in k_data.items() if 'CanExecute' in _v]
             if k_obj.kws.intersection(execute_related_tuple) == set():
                 continue
-            print(k, cannot_execute_names, can_execute_keys)
             for name in all_runtime_names:
                 if name in cannot_execute_names:
                     if len(cannot_execute_names) <= 3:
@@ -488,15 +397,12 @@ def analyze_a_tc_exec_state(dumped_tc_dir):
 
 def analyze_a_tc_only_stack(dumped_tc_dir, th, can_run_runtimes):
     dumped_results = load_results_from_one_dumped_data_dir(dumped_tc_dir)
-    # assert th == len(can_run_runtimes), print(th, len(can_run_runtimes), can_run_runtimes)
     log_counter = Counter()
-    # assert len(dumped_results) == 7
     dumped_results = [r for r in dumped_results if not r.failed_exec]
     if (len(dumped_results)) < 2:
         return []
     for r1_idx in range(len(dumped_results)):
         for r2_idx in range(r1_idx+1, len(dumped_results)):
-            # name_tuple = (dumped_results[r1_idx].name, dumped_results[r2_idx].name)
             if get_diff_attr_names(dumped_results[r1_idx], dumped_results[r2_idx], ['stack_bytes_process_nan']):
                 log_counter[dumped_results[r1_idx].name] += 1
                 log_counter[dumped_results[r2_idx].name] += 1
@@ -552,3 +458,76 @@ def names2counter(d):
     for k in all_runtime_names:
         new_d[k] = len(d.get(k, []))
     return new_d
+
+no_mutation_result_base = '/host_data/rewrite/v18_no_mutation'
+no_mutation_result_base = '/host_data/rewrite/v19.2_no_mutation'
+
+def cal_each_inst_info_nadiff():
+    no_mutation_reason = reasonRelatedC(no_mutation_result_base)
+    rows = ['Variable', 'Memory', 'Vector', 'Reference', 'Table', 'Numeric', 'Parametric', 'Control']
+    runtimes = ['wasmer_default_dump', 'wasmi_interp', 'iwasm_classic_interp_dump', 'iwasm_fast_interp_dump', 'wasm3_dump', 'WasmEdge_disableAOT_newer', 'WAVM_default']
+    title_info = {
+        'Variable': 'Variable instruction',
+        'Memory': 'Memory instruction',
+        'Vector': 'SIMD instruction',
+        'Reference': 'Reference instruction',
+        'Table': 'Table instruction',
+        'Numeric': 'Numeric instruction',
+        'Parametric': 'Parametric instruction',
+        'Control': 'Control instruction',
+        'Total': 'Total'
+    }
+    counters_dict = {}
+    fmt = '{} & {} & {} & {} & {} & {} & {} & {} & {} \\\\'
+    for row_name in rows:
+        counters_dict[row_name] = {}
+
+        can_exec_inst_counter, cannot_exec_inst_counter = no_mutation_reason.cal_execution_diff(row_name,get_ori_names=True)
+        na_diff_inst_names = no_mutation_reason.count_na_diff(row_name, get_ori_names=True)
+        counters_dict[row_name]['na_diff_inst_n'] = na_diff_inst_names
+        counters_dict[row_name]['c_diff_inst_n'],skp_num = no_mutation_reason.count_c_diff(row_name, get_ori_names=True)
+        counters_dict[row_name]['exec_diff_inst_n'],skp_num = no_mutation_reason.count_exec_state_diff(row_name, get_ori_names=True)
+        total_c_diff_inst_n = set()
+        total_na_diff_inst_n = set()
+        total_exec_state_inst_n = set()
+        for runtime in runtimes:
+            total_c_diff_inst_n |= counters_dict[row_name]['c_diff_inst_n'].get(runtime, set())
+            total_na_diff_inst_n |= counters_dict[row_name]['na_diff_inst_n'].get(runtime, set())
+            total_exec_state_inst_n |= counters_dict[row_name]['exec_diff_inst_n'].get(runtime, set())
+
+        counters_dict[row_name]['c_diff_inst_n']['Total'] = total_c_diff_inst_n
+        counters_dict[row_name]['na_diff_inst_n']['Total'] = total_na_diff_inst_n
+        counters_dict[row_name]['exec_diff_inst_n']['Total'] = total_exec_state_inst_n
+
+    runtime_total = {
+        'na_diff_inst_n': Counter({v: 0 for v in runtimes+ ['Total']}),
+        'c_diff_inst_n': Counter({v: 0 for v in runtimes+ ['Total']}),
+        'exec_diff_inst_n': Counter({v: 0 for v in runtimes+ ['Total']})
+    }
+    for row_name in rows:
+        items = []
+        items.append('    ' + title_info[row_name])
+        for runtime in runtimes + ['Total']:
+            na_diff_inst_num = len(counters_dict[row_name]['na_diff_inst_n'].get(runtime, set()))
+            c_diff_inst_num = len(counters_dict[row_name]['c_diff_inst_n'].get(runtime, set()))
+            exec_state_inst_name = len(counters_dict[row_name]['exec_diff_inst_n'].get(runtime, set()))
+            runtime_total['na_diff_inst_n'][runtime] += na_diff_inst_num
+            runtime_total['c_diff_inst_n'][runtime] += c_diff_inst_num
+            runtime_total['exec_diff_inst_n'][runtime] += exec_state_inst_name
+
+            cell = f'{c_diff_inst_num:,} / {exec_state_inst_name:,} / {na_diff_inst_num:,}'
+
+            items.append(cell)
+        print(r'\cmidrule(r){1-9}')
+        print(fmt.format(*items))
+    items = []
+    items.append('    Total')
+    for runtime in runtimes+ ['Total']:
+        cell = f'{runtime_total["c_diff_inst_n"][runtime]:,} / {runtime_total["exec_diff_inst_n"][runtime]:,} / {runtime_total["na_diff_inst_n"][runtime]:,}'
+        items.append(cell)
+    print(r'\cmidrule(r){1-9}')
+    print(fmt.format(*items))
+
+
+if __name__ == '__main__':
+    cal_each_inst_info_nadiff()
